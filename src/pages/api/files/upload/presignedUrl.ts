0915fda8bf9f7d8e277ed75a530e5f3ad745fe05
AlanD <avlasenko@arbat.dev>
2024-09-09 12:21:07 +0300

    update

--- src/pages/api/files/upload/presignedUrl.ts
+++ src/pages/api/files/upload/presignedUrl.ts
@@ -12,10 +12,10 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { userId } = req
 	console.log(req.method)
 
-	if (req.method !== 'POST') {
-		res.status(405).json({ message: 'Only POST requests are allowed' })
-		return
-	}
+	// if (req.method !== 'POST') {
+	// 	res.status(405).json({ message: 'Only POST requests are allowed' })
+	// 	return
+	// }
 
 	const files = req.body as ShortFileProp[]
 
